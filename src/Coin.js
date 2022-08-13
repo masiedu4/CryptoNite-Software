f7ae3fe2120382946b8f2ff7802f912b2c644e89
michaelasiedu148@gmail.com <michaelasiedu148@gmail.com>
2022-08-13 12:50:54 +0000

    refactored tailwind styles

--- src/Coin.js
+++ src/Coin.js
@@ -11,6 +11,14 @@
  * - Modification    :
  **/
 import React from "react";
+const style = {
+  wrapper: "m-5 border border-header p-2 rounded-md",
+  wrapperB: "flex justify-evenly",
+  image: "mx-auto object-cover rounded-full h-6 w-6 ",
+  lowPriceChangeText: "text-red-500 whitespace-nowrap font-semibold",
+  highPriceChangeText: "text-green-500 whitespace-nowrap font-semibold",
+  text: "text-gray-600 whitespace-nowrap font-semibold",
+};
 
 export default function Coin({
   image,
@@ -21,55 +29,42 @@ export default function Coin({
   marketcap,
 }) {
   return (
-    <div className="m-5 border border-header p-2 rounded-md">
+    <div className={style.wrapper}>
       {/* symbol, currency and priceChange */}
-      <div className="flex justify-evenly">
-        <div className="flex justify-evenly">
+      <div className={style.wrapperB}>
+        <div className={style.wrapperB}>
           <a href="#">
-            <img
-              alt="profil"
-              src={image}
-              class="mx-auto object-cover rounded-full h-6 w-6 "
-            />
+            <img alt="profil" src={image} class={style.image} />
           </a>
           <p>{name} </p>
         </div>
         <div>
           {priceChange < 0 ? (
-            <p className="text-red-500 whitespace-nowrap font-semibold">
+            <p className={style.lowPriceChangeText}>
               {" "}
               {priceChange.toFixed(2)}%{" "}
             </p>
           ) : (
-            <p className="text-green-500 whitespace-nowrap font-semibold">
+            <p className={style.highPriceChangeText}>
               {priceChange.toFixed(2)}%{" "}
             </p>
           )}
         </div>
       </div>
       {/* price change */}
-      <div className="flex justify-evenly">
-        <p className="text-gray-600 whitespace-nowrap font-semibold">Price</p>
-        <p className="text-gray-600 whitespace-nowrap font-semibold">
-          ${price.toLocaleString()}
-        </p>
+      <div className={style.wrapperB}>
+        <p className={style.text}>Price</p>
+        <p className={style.text}>${price.toLocaleString()}</p>
       </div>
       {/* volume */}
-      <div className="flex justify-evenly">
-        <p className="text-gray-600 whitespace-nowrap font-semibold">Volume</p>
-        <p className="text-gray-600 whitespace-nowrap font-semibold">
-          ${volume.toLocaleString()}{" "}
-        </p>
+      <div className={style.wrapperB}>
+        <p className={style.text}>Volume</p>
+        <p className={style.text}>${volume.toLocaleString()} </p>
       </div>
       {/* market cap */}
-      <div className="flex justify-evenly">
-        <p className="text-gray-600 whitespace-nowrap font-semibold">
-          {" "}
-          Market Cap{" "}
-        </p>
-        <p className="text-gray-600 whitespace-nowrap font-semibold">
-          ${marketcap.toLocaleString()}{" "}
-        </p>
+      <div className={style.wrapperB}>
+        <p className={style.text}> Market Cap </p>
+        <p className={style.text}>${marketcap.toLocaleString()} </p>
       </div>
     </div>
   );
