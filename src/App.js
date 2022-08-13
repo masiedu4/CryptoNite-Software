68e12e11cd4c1d0506f7c4a267aa43b0cf80830f
michaelasiedu148@gmail.com <michaelasiedu148@gmail.com>
2022-08-13 11:42:23 +0000

    refactoring: used custom hooks for data fetching

--- src/App.js
+++ src/App.js
@@ -10,32 +10,16 @@
  * - Author          : Michael
  * - Modification    :
  **/
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import Header  from "./Header";
+import React, { useState } from "react";
+import Header from "./Header";
 import Coin from "./Coin";
 import FadeIn from "react-fade-in/lib/FadeIn";
-
+import apiData from "./hooks/Data";
 
 export default function App() {
-  const [coins, setCoins] = useState([]);
+  const { coins } = apiData();
+
   const [search, setSearch] = useState("");
-
-  // Get data from API
-
-  useEffect(() => {
-    axios
-      .get(
-        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false`
-      )
-      .then((res) => {
-        setCoins(res.data);
-        console.log(res.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
 
   // set search when input it filtered
 
@@ -50,51 +34,50 @@ export default function App() {
   );
 
   return (
-<FadeIn> 
-    <div className="m-10 sm:m-5"> 
-      
-     <Header search={search} handleChange={handleChange}/> 
-        
-          <div class="container mx-auto px-4 sm:px-8 max-w-3xl">
-            <div class="py-8">
-              <div class="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
-                <div class="inline-block sm:min-w-full shadow rounded-lg overflow-hidden">
-                  <div class="sm:min-w-full leading-normal">
-                    <thead>
-                      <tr class="sm:table-row">
-                        <th
-                          scope="col"
-                          class="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal"
-                        >
-                          Currency
-                        </th>
-                        <th
-                          scope="col"
-                          class="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal"
-                        >
-                          Current Price
-                        </th>
-                        <th
-                          scope="col"
-                          class="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal"
-                        >
-                          Circulating Volume
-                        </th>
-                        <th
-                          scope="col"
-                          class="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal"
-                        >
-                          Price Change
-                        </th>
-                        <th
-                          scope="col"
-                          class="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal"
-                        >
-                          Market Cap
-                        </th>
-                      </tr>
-                    </thead>
- {filteredCoins.map((coin) => ( 
+    <FadeIn>
+      <div className="m-10 sm:m-5">
+        <Header search={search} handleChange={handleChange} />
+
+        <div class="container mx-auto px-4 sm:px-8 max-w-3xl">
+          <div class="py-8">
+            <div class="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
+              <div class="inline-block sm:min-w-full shadow rounded-lg overflow-hidden">
+                <div class="sm:min-w-full leading-normal">
+                  <thead>
+                    <tr class="sm:table-row">
+                      <th
+                        scope="col"
+                        class="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal"
+                      >
+                        Currency
+                      </th>
+                      <th
+                        scope="col"
+                        class="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal"
+                      >
+                        Current Price
+                      </th>
+                      <th
+                        scope="col"
+                        class="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal"
+                      >
+                        Circulating Volume
+                      </th>
+                      <th
+                        scope="col"
+                        class="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal"
+                      >
+                        Price Change
+                      </th>
+                      <th
+                        scope="col"
+                        class="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal"
+                      >
+                        Market Cap
+                      </th>
+                    </tr>
+                  </thead>
+                  {filteredCoins.map((coin) => (
                     <Coin
                       key={coin.id}
                       name={coin.name}
@@ -105,14 +88,13 @@ export default function App() {
                       priceChange={coin.price_change_percentage_24h}
                       marketcap={coin.market_cap}
                     />
-  ))} 
-                  </div>
+                  ))}
                 </div>
               </div>
             </div>
           </div>
-        
-</div>
-</FadeIn>
-  ); 
- }
+        </div>
+      </div>
+    </FadeIn>
+  );
+}
--- src/Coin.js
+++ src/Coin.js
@@ -22,55 +22,53 @@ export default function Coin({
   marketcap,
 }) {
   return (
-    
-              <tbody>
-                <tr>
-                  <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                    <div class="flex items-center">
-                      <div class="flex-shrink-0">
-                        <a href="#" class="block relative">
-                          <img
-                            alt="profil"
-                            src={image}
-                            class="mx-auto object-cover rounded-full h-10 w-10 "
-                          />
-                        </a>
-                      </div>
-                      <div class="ml-3">
-                        <p class="text-gray-900 whitespace-no-wrap">{name} </p>
-                      </div>
-                    </div>
-                  </td>
-                  <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                    <p class="text-gray-900 whitespace-no-wrap">
-                      ${price.toLocaleString()}
-                    </p>
-                  </td>
-                  <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                    <p class="text-gray-900 whitespace-no-wrap">
-                      ${volume.toLocaleString()}
-                    </p>
-                  </td>
-                  <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                    {priceChange < 0 ? (
-                      <p className="text-red-500 whitespace-nowrap">
-                        {" "}
-                        {priceChange.toFixed(2)}%{" "}
-                      </p>
-                    ) : (
-                      <p className="text-green-800 whitespace-nowrap">
-                        {priceChange.toFixed(2)}%{" "}
-                      </p>
-                    )}
-                  </td>
+    <tbody>
+      <tr>
+        <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+          <div class="flex items-center">
+            <div class="flex-shrink-0">
+              <a href="#" class="block relative">
+                <img
+                  alt="profil"
+                  src={image}
+                  class="mx-auto object-cover rounded-full h-10 w-10 "
+                />
+              </a>
+            </div>
+            <div class="ml-3">
+              <p class="text-gray-900 whitespace-no-wrap">{name} </p>
+            </div>
+          </div>
+        </td>
+        <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+          <p class="text-gray-900 whitespace-no-wrap">
+            ${price.toLocaleString()}
+          </p>
+        </td>
+        <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+          <p class="text-gray-900 whitespace-no-wrap">
+            ${volume.toLocaleString()}
+          </p>
+        </td>
+        <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+          {priceChange < 0 ? (
+            <p className="text-red-500 whitespace-nowrap font-bold">
+              {" "}
+              {priceChange.toFixed(2)}%{" "}
+            </p>
+          ) : (
+            <p className="text-green-500 whitespace-nowrap font-bold">
+              {priceChange.toFixed(2)}%{" "}
+            </p>
+          )}
+        </td>
 
-                  <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                    <p class="text-gray-900 whitespace-no-wrap">
-                      ${marketcap.toLocaleString()}
-                    </p>
-                  </td>
-                </tr>
-</tbody>              
-          
+        <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+          <p class="text-gray-900 whitespace-no-wrap">
+            ${marketcap.toLocaleString()}
+          </p>
+        </td>
+      </tr>
+    </tbody>
   );
 }
